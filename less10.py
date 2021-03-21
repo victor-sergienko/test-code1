b963cb13386f9b179bba075c4375c32ebd585a27
victor-sergienko <80215771+victor-sergienko@users.noreply.github.com>
2021-03-21 13:56:24 +0300

    Update less10.py

--- less10.py
+++ less10.py
@@ -1,10 +1,16 @@
+
+# Импортируем функции счетчика, рандома и времени
 from collections import Counter
 import random
 import time
-#Задаем время генерации чисел в 3 секунды
+
+#Задаем время генерации чисел на 3 секунды
 t_end = time.time() + 3
+
+# Объявляем массив, в который сложим числа
 arr = []
 
+# Подбираем рандомно числа, в рамках границ времени
 while time.time() < t_end:
     #print(time.time())
     arr.append(random.randint(0,100))
